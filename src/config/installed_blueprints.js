c3bf5227d477b9e58f6d6840809a53e8025563e2
Crowdbotics <team@crowdbotics.com>
2021-01-12 00:43:59 +0000

    add package blueprint-react-native-sign-in-4-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn41191704', human_name: 'Sign In 4 1', access_route: 'SignIn41191704'},
 { name: 'Dashboard13191702', human_name: 'Dashboard 1 3', access_route: 'Dashboard13191702'},
 
   // you can add more installed blueprints here
